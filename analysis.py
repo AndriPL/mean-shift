dd898a523e86535c1abdb1f0bfd8ee492a616152
Adrian <adiplatos@o2.pl>
2020-06-28 15:10:55 +0200

    feat: add w-statistic and p-value calculations

--- analysis.py
+++ analysis.py
@@ -1,23 +1,23 @@
 from datetime import datetime
 
 import numpy as np
-from scipy.stats import rankdata, ttest_ind
+from scipy.stats import rankdata, ranksums, ttest_ind
 from tabulate import tabulate
 
 from experiment_config import clrs
 
+# Load scores
 scores = np.load(f"./results/scores.npz")
 
-# for measure in scores:
-#     print(f"\n{measure}s:\n", scores[measure].shape)
 
+# Calculate mean_scores
 mean_scores = {}
 for measure in scores:
     # mean_scores = np.mean(scores[measure], axis=2).T
     mean_scores.setdefault(measure, scores[measure].T)
     # print(f"\n{measure} mean_scores:\n", mean_scores[measure])
 
-
+# Calculate ranks
 ranks = {}
 for measure in mean_scores:
     measure_ranks = []
@@ -26,14 +26,31 @@ for measure in mean_scores:
     ranks.setdefault(measure, np.array(measure_ranks))
     print(f"\n{measure} ranks:\n", ranks[measure])
 
+# Calculate mean_ranks
 
-# # Statistical analysis
-# alfa = .05
-# t_statistic = np.zeros((len(clrs.keys()), len(clrs.keys())))
-# p_value = np.zeros((len(clrs.keys()), len(clrs.keys())))
+for measure in ranks:
+    mean_ranks = np.mean(ranks[measure], axis=0)
+    print(f"\n{measure} mean_ranks:\n", mean_ranks)
 
-# mean_scores = 
 
+# Statistical analysis
+alfa = .05
+
+for measure in ranks:
+    w_statistic = np.zeros((len(clrs), len(clrs)))
+    p_value = np.zeros((len(clrs), len(clrs)))
+    for i in range(len(clrs)):
+        for j in range(len(clrs)):
+            w_statistic[i, j], p_value[i, j] = ranksums(ranks[measure].T[i], ranks[measure].T[j])
+    
+    headers = list(clrs.keys())
+    names_column = np.expand_dims(np.array(list(clrs.keys())), axis=1)
+    w_statistic_table = np.concatenate((names_column, w_statistic), axis=1)
+    w_statistic_table = tabulate(w_statistic_table, headers, floatfmt=".2f")
+    p_value_table = np.concatenate((names_column, p_value), axis=1)
+    p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
+    print(f"\n------------------------{measure}----------------------------\n")
+    print("w-statistic:\n", w_statistic_table, "\n\np-value:\n", p_value_table)
 
 # # Results formating
 # headers = clrs.keys()
