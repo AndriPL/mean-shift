61f38064f534e2c0a3c66d5c5cd0f5ec6b685187
Adrian <adiplatos@o2.pl>
2020-06-28 15:17:34 +0200

    feat: add advantage and significance calculations

--- analysis.py
+++ analysis.py
@@ -24,19 +24,21 @@ for measure in mean_scores:
     for score in mean_scores[measure]:
         measure_ranks.append(rankdata(score).tolist())
     ranks.setdefault(measure, np.array(measure_ranks))
-    print(f"\n{measure} ranks:\n", ranks[measure])
+    # print(f"\n{measure} ranks:\n", ranks[measure])
 
 # Calculate mean_ranks
 
 for measure in ranks:
     mean_ranks = np.mean(ranks[measure], axis=0)
-    print(f"\n{measure} mean_ranks:\n", mean_ranks)
+    # print(f"\n{measure} mean_ranks:\n", mean_ranks)
 
 
 # Statistical analysis
 alfa = .05
 
 for measure in ranks:
+
+    # Calculate Wilcoxon statistic and p-value
     w_statistic = np.zeros((len(clrs), len(clrs)))
     p_value = np.zeros((len(clrs), len(clrs)))
     for i in range(len(clrs)):
@@ -49,8 +51,22 @@ for measure in ranks:
     w_statistic_table = tabulate(w_statistic_table, headers, floatfmt=".2f")
     p_value_table = np.concatenate((names_column, p_value), axis=1)
     p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
-    print(f"\n------------------------{measure}----------------------------\n")
-    print("w-statistic:\n", w_statistic_table, "\n\np-value:\n", p_value_table)
+    print(f"\n------------------------{measure}----------------------------")
+    print("\nw-statistic:\n", w_statistic_table, "\n\np-value:\n", p_value_table)
+
+    # Calculate advantage matrix
+    advantage = np.zeros((len(clrs), len(clrs)))
+    advantage[w_statistic > 0] = 1
+    advantage_table = tabulate(np.concatenate(
+        (names_column, advantage), axis=1), headers)
+    print("\nAdvantage:\n", advantage_table)
+
+    # Calculate significance
+    significance = np.zeros((len(clrs), len(clrs)))
+    significance[p_value <= alfa] = 1
+    significance_table = tabulate(np.concatenate(
+        (names_column, significance), axis=1), headers)
+    print("\nStatistical significance (alpha = 0.05):\n", significance_table)
 
 # # Results formating
 # headers = clrs.keys()
